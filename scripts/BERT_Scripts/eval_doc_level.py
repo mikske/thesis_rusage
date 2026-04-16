8d7dc8a2e22df72e31224e9f1f8a8cf6b19fc7a3
Mika Swagemakers <mp.swagemakers@yandex.ru>
2026-04-16 16:06:06 +0300

    падало

--- scripts/BERT_Scripts/eval_doc_level.py
+++ scripts/BERT_Scripts/eval_doc_level.py
@@ -231,7 +231,7 @@ def main():
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    tokenizer = AutoTokenizer.from_pretrained(model_dir)
+    tokenizer = AutoTokenizer.from_pretrained("cointegrated/rubert-tiny2")
     model = AutoModelForSequenceClassification.from_pretrained(model_dir)
     model.to(device)
 
